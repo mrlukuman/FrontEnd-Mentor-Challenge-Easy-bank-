03208dc6462adb6ee0593cdc5f16d11b3fc46634
Tajudeen <4lukumangeneral2@gmail.com>
2022-09-01 21:30:56 +0300

    Added animation to the hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world.")
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', 
+function() {
+    console.log("open hamburgur");
+
+    if (btnHamburger.classList.contains('open')){
+    btnHamburger.classList.remove('open')
+    }else{
+    btnHamburger.classList.add('open')
+    }
+});
